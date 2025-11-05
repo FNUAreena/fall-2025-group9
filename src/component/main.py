32f3f04fc124cea0b4cf94051962bbb6fb822c5b
varshithreddy77 <varshithreddy77@gmail.com>
2025-11-05 00:03:16 -0500

    Update main.py

--- src/component/main.py
+++ src/component/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from tqdm import tqdm
